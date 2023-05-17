243067ea86b22ece96709652d7ef4563d237df20
MaximTvilinev <maximtvilinev@gmail.com>
2023-05-17 10:54:23 +0600

    Initial Commit

---
+++ WorldAtlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldAtlas
+//
+//  Created by Maxim Tvilinev on 17.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
